7a4abc79c71400813cb33bb8f8dc6175af39a0ed
Matilde García <138676242+matildegarcia@users.noreply.github.com>
2023-11-09 13:17:43 -0300

    Update colaDePrioridadJugador.cpp

--- tarea4/src/colaDePrioridadJugador.cpp
+++ tarea4/src/colaDePrioridadJugador.cpp
@@ -14,46 +14,156 @@
 //#include <stdlib.h>
 //#include <string.h>
 
-struct rep_colaDePrioridadJugador {
 
+void intercambio(TJugador &jugador1, TJugador &jugador2);
+void filtradoDescendente(nat pos, TColaDePrioridadJugador &cp);
+
+struct rep_colaDePrioridadJugador {
+    TJugador *arreglo;
+    nat cantidad;
+    bool *pertenece;
+    nat *prioridades; 
+    bool prioridadInvertida; 
 };
 
 TColaDePrioridadJugador crearCP(nat N) {
-  return NULL;
-} // crearCP
+    TColaDePrioridadJugador cp = new rep_colaDePrioridadJugador;
+    cp->arreglo = new TJugador[N+1];
+    cp->cantidad = 0;
+    cp->pertenece = new bool[N + 1];
+    cp->prioridades = new nat[N + 1]; 
+    cp->prioridadInvertida= false;
 
-void invertirPrioridad(TColaDePrioridadJugador &cp) {
-  
+    for (nat i = 0; i <= N; i++) {
+        cp->pertenece[i] = false;
+        cp->prioridades[i] = 0; 
+    }
+    return cp;
 }
 
-void liberarCP(TColaDePrioridadJugador &cp) {
+void invertirPrioridad(TColaDePrioridadJugador &cp) {
+    cp->prioridadInvertida = !cp->prioridadInvertida;
 
-} // liberarCP
+    if (cp->cantidad > 1) {
+        for (nat i = cp->cantidad / 2; i >= 1; i--) {
+            filtradoDescendente(i, cp);
+        }
+    }
+}
+
+   void liberarCP(TColaDePrioridadJugador &cp) {
+    if (cp != NULL) {
+        for (nat i = 1; i <= cp->cantidad; i++) {
+            liberarTJugador(cp->arreglo[i]);
+        }
+        delete[] cp->arreglo;
+        delete[] cp->pertenece;
+        delete[] cp->prioridades;
+        delete cp;
+        cp = NULL; 
+    }
+}
 
 
 void insertarEnCP(TJugador jugador, TColaDePrioridadJugador &cp) {
+    if (estaVaciaCP(cp)) {
+        cp->cantidad++;
+        cp->arreglo[1] = jugador;
+        cp->pertenece[idTJugador(jugador)] = true;
+        cp->prioridades[idTJugador(jugador)] = edadTJugador(jugador);
+    } else {
+        cp->cantidad++;
+        int pos = cp->cantidad;
+        cp->arreglo[pos] = jugador;
+        cp->pertenece[idTJugador(jugador)] = true;
+        cp->prioridades[idTJugador(jugador)] = edadTJugador(jugador);
+        
+        if (!cp->prioridadInvertida){
+        while (pos > 1 && cp->prioridades[idTJugador(cp->arreglo[pos])] < cp->prioridades[idTJugador(cp->arreglo[pos / 2])]) {
+            intercambio(cp->arreglo[pos], cp->arreglo[pos / 2]);
+            pos = pos / 2;
+        }
+        }else{
 
-} // insertarEnCP
-
-bool estaVaciaCP(TColaDePrioridadJugador cp) {
-  return false;
-} // estaVacioCP
-
-TJugador prioritario(TColaDePrioridadJugador cp) {
-  return NULL;
-} // prioritario
-
-void eliminarPrioritario(TColaDePrioridadJugador &cp) {
-
-} // eliminarPrioritario
-
-bool estaEnCP(nat id, TColaDePrioridadJugador cp) {
-  return false;
-} // estaEnCP
-
-nat prioridad(nat id, TColaDePrioridadJugador cp){
-  return 0;
+   while (pos > 1 && cp->prioridades[idTJugador(cp->arreglo[pos])] >= cp->prioridades[idTJugador(cp->arreglo[pos / 2])]) {
+    intercambio(cp->arreglo[pos], cp->arreglo[pos / 2]);
+    pos = pos / 2;
+}
+        }
+    }
 }
 
+void intercambio(TJugador &jugador1, TJugador &jugador2) {
+    TJugador temp = jugador1;
+    jugador1 = jugador2;
+    jugador2 = temp;
+}
 
+bool estaVaciaCP(TColaDePrioridadJugador cp) {
+    return (cp->cantidad == 0); 
+}
 
+TJugador prioritario(TColaDePrioridadJugador cp) {
+    return cp->arreglo[1];
+}
+
+void eliminarPrioritario(TColaDePrioridadJugador &cp) {
+    if (cp->cantidad > 1) {
+        nat id_eliminado = idTJugador(cp->arreglo[1]);
+        cp->pertenece[id_eliminado] = false;
+        liberarTJugador(cp->arreglo[1]); 
+        cp->arreglo[1] = cp->arreglo[cp->cantidad];
+        cp->prioridades[idTJugador(cp->arreglo[1])] = edadTJugador(cp->arreglo[1]);
+        cp->pertenece[idTJugador(cp->arreglo[1])] = true;
+        cp->cantidad--;
+
+        filtradoDescendente(1, cp);
+    } else if (cp->cantidad == 1) {
+        nat id_eliminado = idTJugador(cp->arreglo[1]);
+        cp->pertenece[id_eliminado] = false;
+        liberarTJugador(cp->arreglo[1]); 
+        cp->cantidad--;
+    }
+}
+
+void filtradoDescendente(nat pos, TColaDePrioridadJugador &cp) {
+    nat hijo_izquierdo = 2 * pos;
+    nat hijo_derecho = 2 * pos + 1;
+    nat extremo = pos;
+
+    if (!cp->prioridadInvertida) {
+        if (hijo_izquierdo <= cp->cantidad &&
+            (cp->prioridades[idTJugador(cp->arreglo[hijo_izquierdo])] < cp->prioridades[idTJugador(cp->arreglo[extremo])])) {
+            extremo = hijo_izquierdo;
+        }
+
+        if (hijo_derecho <= cp->cantidad &&
+            (cp->prioridades[idTJugador(cp->arreglo[hijo_derecho])] < cp->prioridades[idTJugador(cp->arreglo[extremo])])) {
+            extremo = hijo_derecho;
+        }
+    } else {
+        if (hijo_izquierdo <= cp->cantidad &&
+            (cp->prioridades[idTJugador(cp->arreglo[hijo_izquierdo])] >= cp->prioridades[idTJugador(cp->arreglo[extremo])])) {
+            extremo = hijo_izquierdo;
+        }
+
+        if (hijo_derecho <= cp->cantidad &&
+            (cp->prioridades[idTJugador(cp->arreglo[hijo_derecho])] >= cp->prioridades[idTJugador(cp->arreglo[extremo])])) {
+            extremo = hijo_derecho;
+        }
+    }
+
+    if (extremo != pos) {
+        intercambio(cp->arreglo[pos], cp->arreglo[extremo]);
+        filtradoDescendente(extremo, cp);
+    }
+}
+
+bool estaEnCP(nat id, TColaDePrioridadJugador cp) {
+   return (cp->pertenece[id]) ;
+  
+}
+
+nat prioridad(nat id, TColaDePrioridadJugador cp) {
+        return cp->prioridades[id];
+}
