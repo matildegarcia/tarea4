befdfa6afc033d9d6fcb7c480f256fd22a8e714f
Matilde García <138676242+matildegarcia@users.noreply.github.com>
2023-11-09 13:16:53 -0300

    Update aplicaciones.cpp

--- tarea4/src/aplicaciones.cpp
+++ tarea4/src/aplicaciones.cpp
@@ -1,6 +1,25 @@
 #include "../include/aplicaciones.h"
 #include "../include/colaDePrioridadJugador.h"
 
-void listarEnOrden(TTablaJugadores t, char** nombres, nat n){
- 
+
+void listarEnOrden(TTablaJugadores t, char** nombres, nat n) {
+    TColaDePrioridadJugador cola = crearCP(100);
+
+    for (nat i = 0; i < n; i++) {
+
+   TJugador jugador =  obtenerJugadorDeTTablaJugadores( t, nombres[i]);
+   
+     if (jugador !=NULL){
+            insertarEnCP(copiarTJugador(jugador), cola);
+
+     }
+            }
+
+    while (!estaVaciaCP(cola)) {
+        TJugador jugador = prioritario(cola);
+        imprimirTJugador(jugador);
+        eliminarPrioritario(cola);
+    }
+
+    liberarCP(cola);
 }
